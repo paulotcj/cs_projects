7972d8aebfd9345bd6bb59bd14986519204e10ca
Paulo Costa <paulotcj@gmail.com>
2023-08-06 20:59:26 -0400

    .

---
+++ parallel_programming/TaskProgramming/TaskProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+static void Write(char c)
+{
+    int i = 1000;
+    while (i-- > 0)
+    {
+        Console.Write(c);
+    }
+}
+
+//-------------------------------
+
+Task.Factory.StartNew(() => Write('.'));
+
+var t = new Task(() => Write('#'));
+t.Start();
+
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
+
+
+
+
