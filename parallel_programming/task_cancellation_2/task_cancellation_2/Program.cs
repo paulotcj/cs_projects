c084c6cc1cfdd81ccd347ce7f6dc641c8d366317
Paulo Costa <paulotcj@gmail.com>
2023-08-08 19:18:56 -0400

    .

---
+++ parallel_programming/task_cancellation_2/task_cancellation_2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var planned = new CancellationTokenSource();
+var preventive = new CancellationTokenSource();
+var emergency = new CancellationTokenSource();
+
+var paranoid = CancellationTokenSource.CreateLinkedTokenSource(planned.Token, preventive.Token, emergency.Token);
+
+Task.Factory.StartNew(() => 
+{
+    int i = 0;
+    while (true)
+    { 
+        paranoid.Token.ThrowIfCancellationRequested();
+        Console.WriteLine(i++);
+        Thread.Sleep(1000);
+    }
+},paranoid.Token);
+Console.ReadKey();
+emergency.Cancel();
+
+
+
+Console.WriteLine("Done.");
+Console.ReadKey();
