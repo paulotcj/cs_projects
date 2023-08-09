57e93fc1ec8bdb917d1416b52dbbc49ce9c188fc
Paulo Costa <paulotcj@gmail.com>
2023-08-08 23:53:52 -0400

    .

---
+++ parallel_programming/exception_handling/exception_handling/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var t1 = Task.Factory.StartNew(() => { 
+    throw new InvalidOperationException("Can't do this") { Source = "T1" };
+});
+
+var t2 = Task.Factory.StartNew(() => {
+    throw new AccessViolationException("Can't access this") { Source = "T2" };
+});
+
+
+try
+{
+    Task.WaitAll(t1, t2);
+}
+catch (AggregateException ae)
+{
+    foreach (Exception e in ae.InnerExceptions)
+    {
+        Console.WriteLine($"Exception {e.GetType()} from {e.Source}.");
+    }
+}
+
+Console.WriteLine("All Done");
+Console.ReadKey();
---
+++ parallel_programming/exception_handling_2/exception_handling_2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+try 
+{
+    DummyFunc();
+}
+catch (AggregateException ae) 
+{
+    foreach (var e in ae.InnerExceptions) 
+    { 
+        Console.WriteLine($"Handled at the initial try/catch at Main. Exception Type: {e.GetType()} - Expected: AccessViolation");
+    }
+}
+
+Console.WriteLine("All Done");
+Console.ReadKey();
+
+static void DummyFunc()
+{
+    var t1 = Task.Factory.StartNew(() => {
+        throw new InvalidOperationException("Can't do this") { Source = "T1" };
+    });
+
+    var t2 = Task.Factory.StartNew(() => {
+        throw new AccessViolationException("Can't access this") { Source = "T2" };
+    });
+
+
+    try
+    {
+        Task.WaitAll(t1, t2);
+    }
+    catch (AggregateException ae)
+    {
+        ae.Handle(e => {
+            if (e is InvalidOperationException)
+            {
+                Console.WriteLine("Invalid Operation - Handled inside DummyFunc()");
+                return true;
+            }
+            else { return false; }
+        });
+
+    }
+}
