b7ba09142cf03d2bbe1d097f510d9ba3682a36c3
Paulo Costa <paulotcj@gmail.com>
2023-08-17 23:49:43 -0400

    .

---
+++ parallel_programming/4_Task_Coordination/5_ManualResetEventSlim_and_AutoResetEvent/5_ManualResetEventSlim_and_AutoResetEvent/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+var evt = new ManualResetEventSlim();
+
+var t1 = Task.Factory.StartNew(() => {
+    Console.WriteLine($"Boiling water");
+    evt.Set();
+});
+
+var t2 = Task.Factory.StartNew(() => {
+    Console.WriteLine($"Waiting for the water to boil....");
+    evt.Wait();
+    Console.WriteLine($"Here's your tea");
+    evt.Wait();
+
+});
+
+t2.Wait();
+Console.WriteLine("-------------------");
+
+
+var evt2 = new AutoResetEvent(false);
+
+t1 = Task.Factory.StartNew(() => {
+    Console.WriteLine($"Boiling water");
+    evt2.Set(); //true
+});
+
+t2 = Task.Factory.StartNew(() => {
+    Console.WriteLine($"Waiting for the water to boil....");
+    evt2.WaitOne(); //false
+    Console.WriteLine($"Here's your tea");
+    var ok = evt2.WaitOne(1000); //false
+
+    if (ok) { Console.WriteLine($"Enjoy your tea."); }
+    else { Console.WriteLine($"Tea crashed at: 0x129823234 - physics stopped working, blackhole starting..."); }
+
+
+});
+
+t2.Wait();
+
+
+Console.WriteLine("-------------------");
+
+
+Console.WriteLine("All Done");
